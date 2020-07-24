a7854504a015649168a4738ece1277d45a8ab361
SanghunKim <sanghun.kim@bhaptics.com>
2020-07-24 16:55:31 +0900

    Add virtual battlegrounds

---
+++ virtual battlegrounds/BallisticHapticObject.cs
@@ -0,0 +1,57 @@
+﻿using Ballistics;
+using Bhaptics.Tact.Unity;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(BallisticObject))]
+public class BallisticHapticObject : MonoBehaviour
+{
+    private BallisticObject ballisticObject;
+
+    [SerializeField] private VestHapticClip HapticClip;
+    [SerializeField] private float targetHeight = 1f;
+
+    private UnityAction<ImpactInfo> OnImpact;
+
+
+    void OnEnable()
+    {
+        OnImpact += OnImpactFunction;
+        ballisticObject = GetComponent<BallisticObject>();
+        ballisticObject.OnImpact.AddListener(OnImpact);
+    }
+
+    private void OnImpactFunction(ImpactInfo arg0)
+    {
+        var velocity = arg0.entryVelocity.magnitude;
+
+        Debug.Log(velocity);
+
+        // use speed for intensity? 
+        var intensity = velocity * 0.001; 
+
+        var angle = BhapticsUtils.Angle(transform.forward, arg0.entryPosition - transform.position);
+
+        if (HapticClip != null)
+        {
+            // HapticClip.Play(1f, 0.5f, 360 - angle, 0);
+
+            // USE OFFSET
+            var offsetY = (arg0.entryPosition.y - transform.position.y) / targetHeight;
+            HapticClip.Play(1f, 0.5f, 360 - angle, offsetY);
+        }
+
+        // Debug.LogError(angle);
+    }
+
+    void Update()
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+        Debug.DrawRay(transform.position, forward, Color.green);
+    }
+
+    void OnDisable()
+    {
+        OnImpact -= OnImpactFunction;
+    }
+}
---
+++ virtual battlegrounds/HapticFiles/Readme.txt
@@ -0,0 +1,11 @@
+Virtual Battleground
+
+비행기 안			Body_Airplane				Recommended [intensity 0.3f], [loop O]
+낙하시(낙하산X)			Body_Skydiving(ParachuteX)			Recommended [intensity 1f], [loop O]
+낙하산 펴질때			Body_Parachute_Active			Recommended [intensity 0.5f], [loop X]
+낙하시(낙하산O)			Body_Skydiving(ParachuteO)			Recommended [intensity 1f], [loop O]
+헤드샷				Head_Hit					Recommended [intensity 1f], [loop X]
+수류탄				Head_Explosion,				Recommended [intensity 1f], [loop X]
+				Body_Explosion, Body_Explosion_back		Recommended [intensity 0.5f], [loop X], Use both same time
+
+(제안)몸샷 더 강하게		Body_Hit					Recommended [intensity 1f], [loop X]
